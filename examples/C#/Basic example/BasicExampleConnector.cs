cf1b8d28a1431fd192587aac13ebe73dad388c4c
Bjornwa <cdz@qlik.com>
2017-08-17 16:14:18 +0200

    Added Basic example for C#

---
+++ examples/C#/Basic example/BasicExampleConnector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Utils;
+using Qlik.Sse;
+
+namespace Basic_example
+{
+    /// <summary>
+    /// The BasicExampleConnector inherits the generated class Qlik.Sse.Connector.ConnectorBase
+    /// </summary>
+    class BasicExampleConnector : Qlik.Sse.Connector.ConnectorBase
+    {
+        public override Task<Capabilities> GetCapabilities(Empty request, ServerCallContext context)
+        {
+            Console.WriteLine("GetCapabilities");
+            return Task.FromResult(new Capabilities
+            {
+                PluginIdentifier = "CSharp Basic example",
+                PluginVersion = "1.0.0",
+                AllowScript = false,
+                Functions =
+                {
+                    new FunctionDefinition {FunctionId = 0, FunctionType = FunctionType.Scalar, Name = "OneConstantPerRow", Params = {new Parameter {Name = "SingleUnusedColumn", DataType = DataType.Dual} }, ReturnType = DataType.Numeric}
+                }                
+            }
+                
+            );
+        }
+
+        public override async Task ExecuteFunction(IAsyncStreamReader<BundledRows> requestStream, IServerStreamWriter<BundledRows> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("ExecuteFunction");
+
+            var requestAsList = await requestStream.ToListAsync(); // We want to be sure to keep the order of rows when executing and writing to the response.
+
+            foreach (var bundledRows in requestAsList)
+            {
+                var resultBundle = new BundledRows();
+                foreach (var row in bundledRows.Rows)
+                {
+                    var resultRow = new Row();
+                    resultRow.Duals.Add(new Dual { NumData = row.Duals[0].NumData + 42.0 });
+                    resultBundle.Rows.Add(resultRow);
+                }
+                await responseStream.WriteAsync(resultBundle);
+            }
+        }
+
+        public override async Task EvaluateScript(IAsyncStreamReader<BundledRows> requestStream, IServerStreamWriter<BundledRows> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("EvaluateScript");
+            await base.EvaluateScript(requestStream, responseStream, context);
+        }
+    }
+}
---
+++ examples/C#/Basic example/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Basic_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grpcHost = "localhost";
+            int grpcPort = 50051;
+            var certificateFolderFullPath =
+                @"..\..\..\..\..\generate_certs_guide\sse_qliktest_generated_certs\sse_qliktest_server_certs";
+
+
+            var sslCredentials = ServerCredentials.Insecure;
+
+            if (certificateFolderFullPath.Length > 3)
+            {
+                var rootCertPath = Path.Combine(certificateFolderFullPath, @"root_cert.pem");
+                var serverCertPath = Path.Combine(certificateFolderFullPath, @"sse_server_cert.pem");
+                var serverKeyPath = Path.Combine(certificateFolderFullPath, @"sse_server_key.pem");
+                if (File.Exists(rootCertPath) &&
+                    File.Exists(serverCertPath) &&
+                    File.Exists(serverKeyPath))
+                {
+                    var rootCert = File.ReadAllText(rootCertPath);
+                    var serverCert = File.ReadAllText(serverCertPath);
+                    var serverKey = File.ReadAllText(serverKeyPath);
+                    var serverKeyPair = new KeyCertificatePair(serverCert, serverKey);
+                    sslCredentials = new SslServerCredentials(new List<KeyCertificatePair>() { serverKeyPair }, rootCert, true);
+
+                    Console.WriteLine($"Path to certificates ({certificateFolderFullPath}) and certificate files found. Opening secure channel with mutual authentication.");
+                }
+                else
+                {
+                    Console.WriteLine($"Path to certificates ({certificateFolderFullPath}) not found or files missing. Opening insecure channel instead.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No certificates defined. Opening insecure channel.");
+            }
+
+            var server = new Grpc.Core.Server
+            {
+                Services = {Qlik.Sse.Connector.BindService(new BasicExampleConnector())},
+                Ports = {new ServerPort(grpcHost, grpcPort, sslCredentials)}
+            };
+
+            server.Start();
+            Console.WriteLine("Press any key to stop Basic example...");
+            Console.WriteLine($"gRPC listening on port {grpcPort}");
+            Console.ReadLine();
+            Console.WriteLine("Shutting Basic example... Bye!");
+            server?.ShutdownAsync().Wait();
+
+        }
+    }
+}
